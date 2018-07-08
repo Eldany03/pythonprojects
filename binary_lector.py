b48fe5d8d5be014bd40e356e7b240d5a5a500bbd
Eldany03 <32372030+Eldany03@users.noreply.github.com>
2018-07-08 18:54:02 -0400

    Binay Lector V1.0 (Beta)

---
+++ binary_lector.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python
+#encodign: latin1
+
+byte = raw_input("Coloque el binario en forma de octeta (byte): ")
+valor = 128
+numero = 0
+for x in range(len(byte)):
+	if "1" == byte[x]:
+		numero += valor
+	else:
+		numero += 0
+	valor = valor / 2
+print "El valor decimal es", numero
